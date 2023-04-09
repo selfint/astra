44ec7ab62e27e020e32ec43ce2496b5c5aa460e0
selfint <selfint@gmail.com>
2023-04-09 22:13:38 +0300

    add reaction to counter

--- astra-core/tests/signal/main.rs
+++ astra-core/tests/signal/main.rs
@@ -2,12 +2,12 @@ use std::ops::{Deref, DerefMut};
 
 use serde::Serialize;
 
-fn build_app<C: Component>(root: C) -> String {
-    let mut count = 0;
-    let (style, html, script) = root.build(|| {
-        count += 1;
-        format!("count{}", count)
-    });
+fn build_app<I, C>(root: C, id_provider: &mut I) -> String
+where
+    I: FnMut() -> String,
+    C: Component<I>,
+{
+    let (style, html, script) = root.build(id_provider);
 
     format!(
         r###"<!DOCTYPE html>
@@ -85,30 +85,81 @@ impl DerefMut for Script {
 }
 
 type Element = (Option<Style>, Html, Option<Script>);
-trait Component {
-    fn build(self, id_provider: impl FnMut() -> String) -> Element;
+trait Component<I: FnMut() -> String> {
+    fn build(self, id_provider: &mut I) -> Element;
 }
 
-impl<F: Fn(&mut Script) -> String> Component for F {
-    fn build(self, id_provider: impl FnMut() -> String) -> Element {
+impl<I, F> Component<I> for F
+where
+    I: FnMut() -> String,
+    F: Fn(&mut I, &mut Script) -> String,
+{
+    fn build(self, id_provider: &mut I) -> Element {
         let mut script = Script(String::new());
-        let html = (self)(&mut script);
+        let html = (self)(id_provider, &mut script);
 
         (None, Html(html), Some(script))
     }
 }
 
-fn counter(script: &mut Script) -> String {
-    r#"<button>0</button>"#.to_string()
+fn counter(id_provider: &mut impl FnMut() -> String, script: &mut Script) -> String {
+    let (count, set_count) = create_signal(&0, id_provider, script);
+
+    let button_id = (id_provider)();
+
+    let button = format!(r#"<button id={button_id}>{count}</button>"#);
+
+    let update_count = set_count(&1);
+
+    script.push_str(&format!(
+        r#"
+{button_id}.addEventListener("click", () => {{
+    {update_count}
+}});
+    "#,
+    ));
+
+    button
 }
 
-fn create_signal<T: Serialize>(id: &str, value: T) -> (String, String) {
-    todo!()
+fn create_signal<T: Serialize>(
+    value: &T,
+    id_provider: &mut impl FnMut() -> String,
+    script: &mut Script,
+) -> (String, impl Fn(&T) -> String) {
+    let initial_value = serde_json::to_string(value).expect("failed to serialize value");
+    let var = (id_provider)();
+    let var_view_id = format!("{}View", var);
+    let set_fn_name = format!("set{}", var);
+
+    script.push_str(&format!(
+        r#"
+let {var} = {initial_value};
+
+function {set_fn_name}(newValue) {{
+    {var} = newValue;
+    {var_view_id}.innerHTML = {var};
+}}
+    "#
+    ));
+
+    (
+        format!(r#"<span id={}>{}</span>"#, var_view_id, initial_value),
+        move |new_value| {
+            let new_value = serde_json::to_string(new_value).expect("failed to serialize value");
+
+            format!("{set_fn_name}({new_value});")
+        },
+    )
 }
 
 #[test]
 fn test_signal() {
-    let app = build_app(counter);
+    let mut count = 0;
+    let app = build_app(counter, &mut || {
+        count += 1;
+        format!("count{count}")
+    });
 
     insta::assert_display_snapshot!(app,
         @r###"
@@ -125,9 +176,20 @@ fn test_signal() {
         
         </style>
 
-        <button id="counter">0</button>
+        <button id=count2><span id=count1View>0</span></button>
 
         <script>
+        
+    let count1 = 0;
+
+    function setcount1(newValue) {
+        count1 = newValue;
+        count1View.innerHTML = count1;
+    }
+        
+    count2.addEventListener("click", () => {
+        setcount1(1);
+    });
         
         </script>
       </body>
